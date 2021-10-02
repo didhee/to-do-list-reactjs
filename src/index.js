d07ee47f8119e8a09e3e82ec5c1314be4b5dae22
Onoriode Dafetta <onoriodedafetta@gmail.com>
2021-10-02 10:46:09 +0100

    initial commit of a to-do list using reactjs

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1
+      style={{
+        textAlign: "center",
+        marginBottom: "0",
+        marginTop: "0",
+        color: " #575353",
+      }}
+    >
+      Made by Didhee
+    </h1>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
